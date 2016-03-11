c00bdcc05184d06d344225eee93d08f9935ca0c8
Flakron Bytyqi <flakron@gmail.com>
2016-03-11 15:39:32 +0100

    initial commit

---
+++ src/main/java/com/devfreaks/teak/TeakApiApplication.java
@@ -0,0 +1,12 @@
+package com.devfreaks.teak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeakApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeakApiApplication.class, args);
+	}
+}
